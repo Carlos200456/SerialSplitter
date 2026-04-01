75192770a7f0d338fbbf43c3759242766ec1cf43
Carlos E. Requejo <carlos@cartech.com.ar>
2026-04-01 16:16:18 -0300

    Funciona Vert y Horizontal Faltan Iconos y Filtro

--- Form1.cs
+++ Form1.cs
@@ -116,8 +116,8 @@ namespace SerialSplitter
             ButtonIrisClose.ButtonImage = Properties.Resources.Iris_Close;
             ButtonVColOpen.ButtonImage = Properties.Resources.ColOpen;
             ButtonVColClose.ButtonImage = Properties.Resources.ColClose;
-            ButtonRotCW.ButtonImage = Properties.Resources.RotCW;
-            ButtonRotCCW.ButtonImage = Properties.Resources.RotCCW;
+            ButtonRotCW.ButtonImage = Properties.Resources.ColOpen;
+            ButtonRotCCW.ButtonImage = Properties.Resources.ColClose;
             ButtonIrisOpen.Location = existingButton1.Location;
             ButtonIrisOpen.Size = existingButton1.Size;
             ButtonIrisOpen.Text = existingButton1.Text;
@@ -158,7 +158,7 @@ namespace SerialSplitter
             ButtonRotCW.Text = existingButton5.Text;
             ButtonRotCW.Font = existingButton5.Font;
             ButtonRotCW.ForeColor = Color.White;
-            ButtonRotCW.BackgroundImage = Properties.Resources.RotCW;
+            ButtonRotCW.BackgroundImage = Properties.Resources.ColOpen;
             ButtonRotCW.BackgroundImageLayout = ImageLayout.Stretch;
             // Put Button Image in front of button text
 
@@ -172,7 +172,7 @@ namespace SerialSplitter
             ButtonRotCCW.Text = existingButton6.Text;
             ButtonRotCCW.Font = existingButton6.Font;
             ButtonRotCCW.ForeColor = Color.White;
-            ButtonRotCCW.BackgroundImage = Properties.Resources.RotCCW;
+            ButtonRotCCW.BackgroundImage = Properties.Resources.ColClose;
             ButtonRotCCW.BackgroundImageLayout = ImageLayout.Stretch;
             ButtonRotCCW.MouseDown += buttonRotCW_MouseDown;
             ButtonRotCCW.MouseUp += buttonRot_MouseUp;
@@ -816,7 +816,7 @@ namespace SerialSplitter
         {
             if (serialPort2.IsOpen)
             {
-                dataOUT2 = "RO-5";
+                dataOUT2 = "HC5";
                 serialPort2.WriteLine(dataOUT2);
             }
         }
@@ -825,10 +825,10 @@ namespace SerialSplitter
         {
             if (serialPort2.IsOpen)
             {
-                dataOUT2 = "RO0";
+                dataOUT2 = "HC0";
                 serialPort2.WriteLine(dataOUT2);
                 Thread.Sleep(100);
-                dataOUT2 = "RO0";
+                dataOUT2 = "HC0";
                 serialPort2.WriteLine(dataOUT2);
             }
         }
@@ -837,7 +837,7 @@ namespace SerialSplitter
         {
             if (serialPort2.IsOpen)
             {
-                dataOUT2 = "RO5";
+                dataOUT2 = "HC-5";
                 serialPort2.WriteLine(dataOUT2);
             }
         }
