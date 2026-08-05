5b39f744fed1fe2c922cb6a53ef1475167834478
Carlos E. Requejo <carlos@cartech.com.ar>
2026-08-05 15:39:12 -0300

    Lapsus en comandos colimador

--- Form1.cs
+++ Form1.cs
@@ -959,15 +959,15 @@ namespace SerialSplitter
                     // No action needed
                     break;
                 case "Overview":
-                    dataOUT2 = "M1";
+                    dataOUT2 = "IM1";
                     serialPort2.WriteLine(dataOUT2);
                     break;
                 case "Mag1":
-                    dataOUT2 = "M2";
+                    dataOUT2 = "IM2";
                     serialPort2.WriteLine(dataOUT2);
                     break;
                 case "Mag2":
-                    dataOUT2 = "M3";
+                    dataOUT2 = "IM3";
                     serialPort2.WriteLine(dataOUT2);
                     break;
                 default:
